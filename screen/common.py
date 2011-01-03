e46f2bcdf8a85093c2b58b1f11be09b0469cc0b5
Henrik Forstén <henrik.forsten@gmail.com>
2011-01-03 15:56:15 +0200

    Existing code should work on windows so moved it to common file

--- screen/common.py
+++ screen/common.py
@@ -1,15 +1,13 @@
 import PIL.Image
-import sys
 import config
+import gtk.gdk
 
-if sys.platform == 'darwin':
-    raise NotImplementedError
-
-if sys.platform == 'win32':
-    raise NotImplementedError
-
-else:
-    from linux import *
+"""Sets up some global variables.
+size is tuple of screen size.
+root is handle to accessing display.""" 
+root = gtk.gdk.get_default_root_window()
+size = gtk.gdk.get_default_root_window().get_size()
+colormap = gtk.gdk.colormap_get_system()
     
 class DTM():
     """DTM is array of pixel locations and colors
@@ -36,6 +34,43 @@ class DTM():
         for i,j in self.pixels.items():
             yield i,j
             
+            
+def getArea(x1,y1,x2,y2):
+    """Return area from desktop.
+    Thanks to FionaSarah for the code."""
+    global root, colormap
+    x1+=config.ox
+    y1+=config.oy
+    x2+=config.ox
+    y2+=config.oy
+    x,y=x2-x1,y2-y1
+    screengrab = gtk.gdk.Pixbuf(
+        gtk.gdk.COLORSPACE_RGB,
+        False,
+        8,
+        x,
+        y
+      )
+
+    screengrab.get_from_drawable(
+        root, 
+        colormap,
+        x1, y1, 0, 0,
+        x,
+        y
+      )
+
+    final_screengrab = PIL.Image.frombuffer(
+      "RGB",
+      (x, y),
+      screengrab.get_pixels(),
+      "raw",
+      "RGB",
+      screengrab.get_rowstride(),
+      1
+    )
+    return final_screengrab
+
 def getScreenSize():
     return size
     
